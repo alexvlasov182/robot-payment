6d95be160634bc9c8c26f362138ca877723d4ee3
Oleksandr Vlasov <drumlife182@gmail.com>
2026-07-19 16:27:35 +0200

    fix: entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "🔄 Running Alembic migrations..."
-alembic upgrade head 2>/dev/null || alembic stamp head
+alembic upgrade head >/dev/null 2>&1 || alembic stamp head >/dev/null 2>&1
 
 echo "🚀 Starting Gunicorn..."
 exec gunicorn app.main:app --workers 2 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000
